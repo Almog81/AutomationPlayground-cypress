adfb4fec7494c6d371b7138e43223a97414d0cd0
Almog81 <almog81@gmail.com>
2023-06-18 19:56:15 +0300

    add Test09

--- cypress/e2e/AutomationPlayground.cy.js
+++ cypress/e2e/AutomationPlayground.cy.js
@@ -31,6 +31,59 @@ describe('Automation Playground', () => {
   it('Test05: AJAX Data', () => {
     cy.contains('AJAX Data').click()
     cy.get('.btn-primary').click()
-    cy.get('bg-success', {timeout:20000}).should('have.text', 'Data loaded with AJAX get request.')
+    cy.get('.bg-success', {timeout:20000}).should('have.text', 'Data loaded with AJAX get request.')
   })
+
+  it('Test05: Client Side Delay', () => {
+    cy.contains('Client Side Delay').click()
+    cy.get('.btn-primary').click()
+    cy.get('.bg-success', {timeout:20000}).should('have.text', 'Data calculated on the client side.')
+  })
+
+  it('Test06: Click', () => {
+    cy.contains('Click').click()
+    cy.get('.btn-primary').click()
+    cy.get('.btn-success').should('exist')
+  })
+
+  it('Test07: Text Input', () => {
+    cy.contains('Text Input').click()
+    cy.get('.form-control').type('KUKU')
+    cy.get('.btn-primary').click()
+    cy.get('.btn-primary').should('have.text', 'KUKU')
+  })
+
+  it('Test08: Scrollbars', () => {
+    cy.contains('Scrollbars').click()
+    cy.get('.btn-primary').scrollIntoView().click()
+  })
+
+  it.only('Test09: Dynamic Table', () => {
+    cy.contains('Dynamic Table').click();
+  
+    cy.get('.bg-warning')
+      .invoke('text')
+      .then((text) => {
+        const expected = text.split(': ')[1];
+        cy.log(expected)
+        cy.contains('span[role="columnheader"]', 'CPU')
+          .invoke('index')
+          .then((x) => {
+            cy.log('x = ' + x)
+            cy.contains('span[role="cell"]', 'Chrome')
+              .invoke('index')
+              .then((y) => {
+                cy.log('y = ' + y)
+                cy.log('x+y = ' + (x+y))
+                cy.get(`span[role="cell"]:eq(${y + x})`)
+                  .invoke('text')
+                  .then((result) => {
+                    expect(expected).to.equal(result);
+                  });
+              });
+          });
+      });
+  });
+  
+
 })
