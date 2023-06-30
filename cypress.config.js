e603705fdc00368d66b8497c3814e5d90fb4bab5
Almog81 <almog81@gmail.com>
2023-06-30 11:43:05 +0300

    add Mochawesome-Report

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,19 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      charts: true,
+      overwrite: false,
+      html: false,
+      json: true,
+      reportDir: "./cypress/report/mochawesome-report/",
+      reportFilename: "[name]-[datetime]-[status]",
+      timestamp: "longDate"
+    },  
     e2e: {
         setupNodeEvents(on, config) {
-            // implement node event listeners here
+            require('cypress-mochawesome-reporter/plugin')(on);
         },
     },
 });
